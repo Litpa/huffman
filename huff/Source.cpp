066844fe219a6f7f83f54a30f6d9c7fe4a6209c8
Litpa <aslitpa@gmail.com>
2020-03-01 20:31:03 +0700

    Вычисление частот символов в исходном файле.

--- huff/Source.cpp
+++ huff/Source.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 int main() {
-	cout << "test2";
+	//todo: take the filename the from console
+	int frq[256];
+	for (int i = 0; i < 256; i++) {
+		frq[i] = 0;
+	}
+	ifstream f("input.txt");
+	if (!f) {
+		cerr << "File not found\n";
+		exit(1);
+	}
+	char inp;
+	while (f >> inp) {
+		frq[inp] += 1;
+	}
+	f.close();
 	return 0;
 }
